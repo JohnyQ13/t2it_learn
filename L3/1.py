2ca5d67f990b09b00df6ee9cc2f65f1d4cfe39cb
JohnyQ13 <igormaystruk13@gmail.com>
2023-04-07 18:04:23 +0300

    hw3

---
+++ L3/1.py
@@ -0,0 +1,8 @@
+x = int(input('Input number:'))
+
+if not x:
+    print('Zero not ODD not EVEN')
+elif (x%2):
+    print('It is ODD')
+else:
+    print('It is EVEN')
---
+++ L3/2.py
@@ -0,0 +1,6 @@
+x = int(input('Input number:'))
+
+if (x%2 and x%3 and x%5):
+    print("It is Good!")
+else:
+    print("It is NOT Good",x%2)
