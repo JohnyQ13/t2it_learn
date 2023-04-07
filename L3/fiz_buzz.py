6f6b2e37d1217e85b7496491e62a80b5a4e6b3c6
my_notebook <igormaystruk13@gmail.com>
2023-04-07 20:11:52 +0300

    add check input

--- L3/fiz_buzz.py
+++ L3/fiz_buzz.py
@@ -1,8 +1,27 @@
-fizz = int(input('Input fizz:'))
-buzz = int(input('Input buzz:'))
-limit = int(input('input limit:'))
+def is_positive_number(num):
+    try:
+        num = int(num)
+        if num > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
 
-for i in range(1,limit):
+while True:
+    fizz = int(input('Input fizz:'))
+    buzz = int(input('Input buzz:'))
+    limit = int(input('input limit:'))
+
+    if is_positive_number(fizz) and is_positive_number(buzz) and is_positive_number(limit):
+        fizz = int(fizz)
+        buzz = int(buzz)
+        limit = int(limit)
+        break
+    else:
+        print('ERROR: Input positive number')
+
+for i in range(1,limit+1):
     if (not i%fizz and not i%buzz):
         print('FB', end=' ')
     elif not i % fizz:
